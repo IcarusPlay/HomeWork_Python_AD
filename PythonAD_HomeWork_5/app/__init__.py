002b75049b5f1a8db118b46adac4a2a13e5bb5e8
Bogdan_Puchkov <bogdanicarus659@gmail.com>
2026-04-29 12:12:42 +0200

    Add HomeWork 5

---
+++ PythonAD_HomeWork_5/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from app.routers.questions import questions_bp
+from app.routers.responses import responses_bp
+from config import DevelopmentConfig
+from app.extensions import db, migrate
+import app.models
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    app.register_blueprint(questions_bp, url_prefix='/questions')
+    app.register_blueprint(responses_bp, url_prefix='/responses')
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
